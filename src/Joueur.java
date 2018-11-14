19e2242ee0fb1507955c2a3f20f5b25c81bc319a
N07070 <->
2018-11-14 17:18:00 +0100

    Ajout de la classe Joueur et début de l'écriture du code.

---
+++ src/Joueur.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+class Joueur {
+
+	private final int numeroDeJoeur;
+	private static int nbrJoueurs;
+	private int nbrDePoints;
+	private LinkedList<Domino> mainDuJoeur;
+
+	public Joueur(LinkedList<Domino> piocheDeDepart){
+		this.numeroDeJoeur = this.nbrJoueurs++;
+		this.nbrDePoints = 0;
+		this.mainDuJoeur = piocheDeDepart;
+	}
+
+	public int getNbrDePoints(){
+		return this.nbrDePoints;
+	}
+
+	public int getNombrePiecesRestantes(){
+		return this.mainDuJoeur.size();
+	}
+
+	public int getDominoAuPif(){
+		return this.mainDuJoeur.get(Random.nextInt(this.mainDuJoeur.size()));
+	}
+
+	public int getDominoNumero(int i){
+		return this.mainDuJoeur.get(i);
+	}
+
+	public int getNumeroDeJoueur(){
+		return this.numeroDeJoeur;
+	}
+}
