d496ad1689bb5f32a0cf62a91d702ee59aff4878
N07070 <->
2018-11-26 11:56:39 +0100

    Ajout de quelques fichiers

---
+++ src/Jouer.java
@@ -0,0 +1,16 @@
+public class Jouer {
+	Joueur joueurListe[];
+	int tourCourant;
+
+	public Jouer(int nbrJoueurs){
+		// Creer le nombre de joueur
+		if (nbrJoueurs) {
+
+		}
+		for (int i = 0 ; i < nbrJoueurs ; ) {
+
+		}
+		// Initialiser le tour courant
+	}
+
+}
