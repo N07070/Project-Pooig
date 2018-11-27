80a6cf7fddc2b750958fda39c9b79054ff67eb57
Mehdi <mehdibekhtaoui15@hotmail.com>
2018-11-27 19:14:59 +0100

    Modif de la classe Plateau

--- src/Plateau.java
+++ src/Plateau.java
@@ -3,26 +3,20 @@ import java.util.*;
 public class Plateau {
 
   // La taille du plateau est composé d'une hauteur, et d'un longueur.
-  private int hauteur;
-  private int longueur;
+  private int taille;
 
   // le plateau contient une série de dominos.
   private ArrayList <Domino> dominos;
 
   /* On créant le plateau, on fiche sa hauteur et sa largeur,
   et on crée une liste vide de dominos qu'on complétera au fur et à mesure de l'avancement du jeu. */
-  public Plateau (int h, int l){
-    this.hauteur = h;
-    this.longueur = l;
+  public Plateau (int t){
+    this.taille = t;
     this.dominos = new ArrayList <Domino>();
   }
 
-  public int getHauteur(){
-    return this.hauteur;
-  }
-
-  public int getLongueur(){
-    return this.longueur;
+  public int getTaille(){
+    return this.taille;
   }
 
   // Détermine si le plateau est vide ou pas.
@@ -32,10 +26,10 @@ public class Plateau {
 
   // Fonction qui vérifie que le domino est bien dans le plateau.
   public boolean positionEstBonne(Domino d){
-    if (d.getPosition()[0][0] >= 0 && d.getPosition()[0][0] <= longueur
-    && d.getPosition()[0][1] >= 0 &&  d.getPosition()[0][1] <= hauteur) {
-      if(d.getPosition()[1][0] >= 0 && d.getPosition()[1][0] <= longueur
-      && d.getPosition()[1][1] >= 0 && d.getPosition()[1][1] <= hauteur){
+    if (d.getPosition()[0][0] >= 0 && d.getPosition()[0][0] <= taille
+    && d.getPosition()[0][1] >= 0 &&  d.getPosition()[0][1] <= taille) {
+      if(d.getPosition()[1][0] >= 0 && d.getPosition()[1][0] <= taille
+      && d.getPosition()[1][1] >= 0 && d.getPosition()[1][1] <= taille){
         if((d.getPosition()[1][0] == d.getPosition()[0][0] -1 || d.getPosition()[1][0] == d.getPosition()[0][0] +1)
         && (d.getPosition()[1][1] == d.getPosition()[0][1] -1 || d.getPosition()[1][1] == d.getPosition()[0][1] +1)){
           return true;
